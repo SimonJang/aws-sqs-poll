dc48d096c52d93a86b54129e221e90ff7ef2201e
Simon <simon.jang.contact@gmail.com>
2017-04-24 21:40:58 +0200

    added options, extra test

--- index.js
+++ index.js
@@ -6,21 +6,26 @@ const sqs = new AWS.SQS();
 const getQueueUrl = pify(sqs.getQueueUrl.bind(sqs));
 const poll = pify(sqs.receiveMessage.bind(sqs));
 
-module.exports = (queueName, queueOwnerId) => {
+module.exports = (queueName, options) => {
+	options = Object.assign({
+		awsAccountId: process.env.AWS_ACCOUNT_ID,
+		MaxNumberOfMessages: 10
+	}, options);
+
 	if (!queueName) {
 		return Promise.reject(new TypeError('Please provide a queue name'));
 	}
 	if (queueName.length > 80 || !/^[a-zA-Z0-9_-]{1,80}$/i.test(queueName)) {
 		return Promise.reject(new TypeError('Invalid queue name'));
 	}
-	if (!queueOwnerId || queueOwnerId.length !== 12 || !/([0-9]{12})+/i.test(queueOwnerId)) {
+	if (!options.awsAccountId || options.awsAccountId.length !== 12 || !/([0-9]{12})+/i.test(options.awsAccountId)) {
 		return Promise.reject(new TypeError('Invalid queueOwnerId'));
 	}
 
 	return getQueueUrl(
 		{
 			QueueName: queueName,
-			QueueOwnerId: queueOwnerId
+			QueueOwnerId: options.awsAccountId
 		}
 	).then(urlQueueResponse => {
 		if (urlQueueResponse.QueueUrl && urlQueueResponse.QueueUrl.length !== 0) {
@@ -30,7 +35,7 @@ module.exports = (queueName, queueOwnerId) => {
 	}).then(url => {
 		return {
 			QueueUrl: url,
-			MaxNumberOfMessages: 10
+			MaxNumberOfMessages: options.MaxNumberOfMessages
 		};
 	}).then(params => poll(params));
 };
--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 import test from 'ava';
 import sinon from 'sinon';
+
 import sqs from './fixtures/fake-sqs';
 import lib from './';
 
 const sandbox = sinon.sandbox.create();
 
 test.before(() => {
+	sandbox.stub(process, 'env', {AWS_ACCOUNT_ID: '123456789111'});
 	const stubGetQueue = sandbox.stub(sqs, 'getQueueUrl');
 	stubGetQueue.withArgs({QueueName: 'demoQueue', QueueOwnerAWSAccountId: '123456789111'}).yields(undefined, {QueueUrl: 'https://sqs.eu-west-1.amazonaws.com/12345678912/somequeue'});
 	stubGetQueue.withArgs({QueueName: 'unknownQueue', QueueOwnerAWSAccountId: '123456789111'}).yields(undefined, {QueueUrl: ''});
@@ -23,20 +25,26 @@ test('error no queue name', async t => {
 });
 
 test('invalid queue name test', async t => {
-	t.throws(lib('l[7777&&]l', '123456789111'), 'Invalid queue name');
-});
-
-test('invalid queueOwnerId test', async t => {
-	t.throws(lib('demoQueue', '12345'), 'Invalid queueOwnerId');
+	t.throws(lib('l[7777&&]l'), 'Invalid queue name');
 });
 
 test('invalid queue not found test', t => {
-	t.throws(lib('unknownQueue', '123456789111'), 'Queue not found');
+	t.throws(lib('unknownQueue'), 'Queue not found');
 });
 
 test('checking messages reponse', async t => {
-	const response = await lib('demoQueue', '123456789111');
+	const response = await lib('demoQueue');
 	const parsedArray = JSON.stringify(['message1', 'message2', 'message3']);
 	t.true(response.Messages.length !== 0);
 	t.is(JSON.stringify(response.Messages), parsedArray);
+});
+
+test('checking message response is promise', async t => {
+	const response = lib('demoQueue');
+	t.true(response instanceof Promise);
+});
+
+test('invalid queueOwnerId test', async t => {
+	sandbox.stub(process, 'env', {AWS_ACCOUNT_ID: '123'});
+	t.throws(lib('demoQueue'), 'Invalid queueOwnerId');
 });
