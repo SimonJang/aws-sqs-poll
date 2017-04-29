db1a3ab5e369eedf43b5b723563a98dd87e30f29
Sam Verschueren <sam.verschueren@gmail.com>
2017-04-29 19:46:36 +0200

    fix tests

--- test/test.js
+++ test/test.js
@@ -1,32 +1,25 @@
-import sqs from './fixtures/fake-sqs';
 import test from 'ava';
-import sinon from 'sinon';
+import './fixtures/fake-sqs';		// eslint-disable-line import/no-unassigned-import
 import m from '../';
 
-const sandbox = sinon.sandbox.create();
-
-test.after(() => {
-	sandbox.restore();
+test('error no queue name', async t => {
+	await t.throws(m(), 'Expected `queueName` to be of type `string`, got `undefined`');
 });
 
-test('error no queue name', t => {
-	t.throws(m(), 'Expected `queueName` to be of type `string`, got `undefined`');
+test('invalid queue name test', async t => {
+	await t.throws(m('l[7777&&]l'), 'Invalid queue name');
 });
 
-test('invalid queue name test', t => {
-	t.throws(m('l[7777&&]l'), 'Invalid queue name');
-});
-
-test('invalid queueOwnerId test', t => {
-	t.throws(m('demoQueue', {awsAccountId: '1vavaf2'}), 'Invalid AWS Account Id');
+test('invalid queueOwnerId test', async t => {
+	await t.throws(m('demoQueue', {awsAccountId: '1vavaf2'}), 'Invalid AWS Account Id');
 });
 
 test('invalid queue not found test', async t => {
-	t.throws(m('trolololo', {awsAccountId: '123456789111'}), 'Queue `trolololo` not found');
+	await t.throws(m('trolololo', {awsAccountId: '123456789111'}), 'Queue `trolololo` not found');
 });
 
 test('returns promise', async t => {
 	const result = await m('demoQueue', {awsAccountId: '123456789111'});
-	const messages = {Messages: ['message1', 'message2', 'message3']}
+	const messages = {Messages: ['message1', 'message2', 'message3']};
 	t.deepEqual(result, messages.Messages);
-})
+});
