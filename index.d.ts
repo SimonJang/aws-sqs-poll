5ef6045c69644df76d987499f0692bf3615a26a7
Simon Jang <simon.jang.contact@gmail.com>
2020-07-04 09:13:05 +0200

    Add types

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+interface Options {
+	/**
+	 * Duration in seconds for which the call waits for a message to arrive from the queue before returning
+	 */
+	timeout: number;
+	/**
+	 * Maximum number of messages to return. SQS might return fewer messages even if the maximum number is available
+	 */
+	numberOfMessages: number;
+	/**
+	 * AWS Account id
+	 */
+	awsAccountId?: string;
+	/**
+	 * Flag to indicate that the messages need to be returned a JSON and not as raw strings
+	 */
+	json?: boolean;
+}
+
+/**
+ * Poll an SQS queue for messages
+ *
+ * @param queueName - Name of the queue
+ * @param options - Polling options
+ * @returns A array of messages
+ */
+declare function awsSQSPOll<T = unknown>(queueName: string, options: Options): Promise<T>;
+
+export = awsSQSPOll;
---
+++ index.test-d.ts
@@ -0,0 +1,18 @@
+import {expectType} from 'tsd';
+import awsSQSPoll = require('.');
+
+expectType<Promise<unknown[]>>(
+	awsSQSPoll('somequeue', {awsAccountId: '12324412132', json: true, numberOfMessages: 10, timeout: 10})
+);
+
+expectType<Promise<unknown[]>>(
+	awsSQSPoll('somequeue', {awsAccountId: '12324412132', numberOfMessages: 10, timeout: 10})
+);
+
+expectType<Promise<unknown[]>>(
+	awsSQSPoll('somequeue', {json: true, numberOfMessages: 10, timeout: 10})
+);
+
+expectType<Promise<unknown[]>>(
+	awsSQSPoll('somequeue', {numberOfMessages: 10, timeout: 10})
+);
